c17ad41cd491a7c85a1203b307084ca30601615e
wsdjj <76938752+wsdjj@users.noreply.github.com>
2021-03-13 17:39:24 +0800

    Create bububaoCOOKIE.js

---
+++ bububaoCOOKIE.js
@@ -0,0 +1,13 @@
+//独立COOKIE文件     ck在``里面填写，多账号换行
+
+let bububaotokenVal = `DB0358470A222AD198EEFEA46525690G1611024616
+93931264618C66C63B41D7E9A533220G1615628082`
+
+
+
+let bububaocookie = {
+    bububaotokenVal: bububaotokenVal,
+
+}
+
+module.exports = bububaocookie
